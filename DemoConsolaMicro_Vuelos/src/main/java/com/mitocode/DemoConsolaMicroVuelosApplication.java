b24ce90e5f30a5a8339471fbb8337407c40cdf1a
Brahian <bastudillomeza@gmail.com>
2020-04-16 03:15:16 -0500

    Creamos un nuevo proyecto

---
+++ DemoConsolaMicro_Vuelos/src/main/java/com/mitocode/DemoConsolaMicroVuelosApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsolaMicroVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsolaMicroVuelosApplication.class, args);
+	}
+
+}
