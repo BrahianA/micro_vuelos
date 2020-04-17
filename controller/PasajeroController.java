edf36428c59ca47a118f43f207c0e884409da07a
Brahian <bastudillomeza@gmail.com>
2020-04-16 21:04:24 -0500

    creamos un paquete llamado controller y dentro de ese paquete creamos una clase llamada PasajeroController y creamos el codigo para listar los pasajeros

---
+++ controller/PasajeroController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.Pasajero;
+import com.example.demo.service.PasajeroServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class PasajeroController {
+
+	@Autowired
+	PasajeroServiceImpl pasajeroServiceImpl;
+	
+	@GetMapping("/pasajeros")
+	public List<Pasajero> listarPasajero(){
+		return pasajeroServiceImpl.listarPasajeros();
+	}
+	
+}
