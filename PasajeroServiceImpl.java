28b2d9b1718a247a7cfb73ccc1f5f51cc9f2a103
Brahian <bastudillomeza@gmail.com>
2020-04-17 21:59:39 -0500

    Implementamos el metodo guardar

---
+++ PasajeroServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.dao.IPasajeroDAO;
+import com.example.demo.dto.Pasajero;
+
+@Service
+public class PasajeroServiceImpl implements IPasajeroService {
+	
+	@Autowired
+	IPasajeroDAO iPasajeroDAO;
+	
+	@Override
+	public List<Pasajero> listarPasajeros() {
+		return iPasajeroDAO.findAll();
+	}
+
+	@Override
+	public Pasajero guardarPasajero(Pasajero pasajero) {
+		
+		return iPasajeroDAO.save(pasajero);
+	}
+
+	@Override
+	public Pasajero clientePorId(Long numeroIdentificacion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pasajero actualizarPasajero(Pasajero pasajero) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarPasajero(Long numeroIdentificacion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
