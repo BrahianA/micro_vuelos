3cbaf506849e82d6ab40900460e66b0cca134d48
Brahian <bastudillomeza@gmail.com>
2020-04-18 01:53:00 -0500

    hacemos el llamado del metodo actualizar pasajero

--- PasajeroController.java
+++ PasajeroController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,27 @@ public class PasajeroController {
 		System.out.println("Pasajero seleccionado: "+ pasajero_xid);
 		
 		return pasajero_xid;
+	}
+	
+	@PutMapping("/pasajeros/{numeroIdentificacion}")
+	public Pasajero actualizarPasajero (@PathVariable (name ="numeroIdentificacion") Long numeroIdentificacion,@RequestBody Pasajero pasajero) {
+		
+		Pasajero pasajero_seleccionado=new Pasajero();
+		Pasajero pasajero_actualizado=new Pasajero();
+		
+	    pasajero_seleccionado=pasajeroServiceImpl.clientePorId(numeroIdentificacion);
+	    pasajero_seleccionado.setNombres(pasajero.getNombres());
+	    pasajero_seleccionado.setApellidos(pasajero.getApellidos());
+	    pasajero_seleccionado.setCorreo(pasajero.getCorreo());
+	    pasajero_seleccionado.setTelefono(pasajero.getTelefono());
+	    
+	    pasajero_actualizado=pasajeroServiceImpl.actualizarPasajero(pasajero_seleccionado);
+		
+	    System.out.println("El pasajero actualizado es: "+ pasajero_actualizado);
+	    
+	    return  pasajero_actualizado;
+		
+		
+		
 	}
 }
